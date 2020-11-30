4a8a9b755388332da3ddad6e111f96695c582745
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-11-29 22:46:22 -0300

    [NEW] criação de rotas e controllers para verificação de disponibilidade do provider

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,23 @@
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+export default class ProviderDayAvailabilityController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { day, month, year } = request.body;
+
+    const listProviderDayAvailability = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const availabilty = await listProviderDayAvailability.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json(availabilty);
+  }
+}

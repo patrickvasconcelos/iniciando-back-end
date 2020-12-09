b5e9b87081ec013c0eefc75592a16a9f16074f2e
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-12-08 23:06:01 -0300

    [NEW] pequenos ajustes

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,4 @@
-import { getDate, getDaysInMonth } from 'date-fns';
+import { getDate, getDaysInMonth, isAfter } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -32,7 +32,6 @@ class ListProviderMonthAvailabilityService {
         year,
       },
     );
-
     const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
     const eachDayArray = Array.from(
@@ -40,12 +39,14 @@ class ListProviderMonthAvailabilityService {
       (_, index) => index + 1,
     );
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
       const appointmentsInDay = appointments.filter(
         appointment => getDate(appointment.date) === day,
       );
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
